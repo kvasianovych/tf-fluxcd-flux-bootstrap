0e8e07da3861f12e2fb9e2801c483de22b468924
Kostiantyn Vasianovych <k.vasianovych@pm.me>
2026-01-03 22:04:21 +0200

    Add versions.tf

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.14"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.14.1"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = "~> 1.7.6"
+    }
+  }
+}
